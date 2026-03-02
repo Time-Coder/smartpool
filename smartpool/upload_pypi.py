58c82d6d76295b8dbcee416c9062b85f18711012
Time-Coder <binghui.wang@foxmail.com>
2026-03-02 21:33:15 +0800

    add root README.md

--- smartpool/upload_pypi.py
+++ smartpool/upload_pypi.py
@@ -4,4 +4,4 @@ import os
 
 
 self_folder = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
-subprocess.call([sys.executable, "-m", "twine", "upload", f"{self_folder}/dist/smartpool-*.tar.gz", f"{self_folder}/dist/smartpool-*.whl", "--verbose"])
+subprocess.call([sys.executable, "-m", "twine", "upload", f"{self_folder}/dist/pysmartpool-*.tar.gz", f"{self_folder}/dist/pysmartpool-*.whl", "--verbose"])
